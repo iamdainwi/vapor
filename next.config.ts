4bb7131998f657d5a4f90b4c455e3b423e6e2d47
iamdanwi <danwichoudhary@gmail.com>
2026-03-26 12:07:47 +0530

    fix: turbopack build issue fixed

--- next.config.ts
+++ next.config.ts
@@ -6,7 +6,6 @@ const withPWA = withPWAInit({
   // CRITICAL: Stops the service worker from caching your local dev environment
   // disable: process.env.NODE_ENV === "development",
   register: true,
-  skipWaiting: true,
 });
 
 const nextConfig: NextConfig = {
